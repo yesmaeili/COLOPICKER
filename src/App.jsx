210f48fde6472f0dcd813a084894bbe04d07569e
Yasaman Esmaeili <yasamanesmaeili@Yasamans-MacBook-Air.local>
2023-07-06 23:48:56 -0400

    more updates added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,54 @@
-// Write your Color component here
+import React, { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
-const App = () => {
+const Color = ({ color, setSelectedColor }) => {
   return (
-    <div id="container">
-      <div id="navbar">
-        <div>Currently selected: </div>
-        <div className="red">red</div>
-      </div>
-      <div id="colors-list">{/* colors go here */}</div>
-    </div>
+    <div
+      className={color}
+      onClick={() => setSelectedColor(color)}
+    ></div>
   );
 };
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [selectedColor, setSelectedColor] = useState("");
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <div id="colors-list">
+        <Color color="red" setSelectedColor={setSelectedColor} />
+        <Color color="blue" setSelectedColor={setSelectedColor} />
+        <Color color="green" setSelectedColor={setSelectedColor} />
+      </div>
+      <div id="navbar">
+        <div>Currently selected: </div>
+        <div className={selectedColor}>{selectedColor}</div>
+      </div>
+    </>
+  );
+}
 
 export default App;
